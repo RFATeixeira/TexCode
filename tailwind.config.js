d762c8a725dc23154cb3bd143614fee15873075c
RFATeixeira <RCSTeixeira@hotmail.com>
2024-07-01 16:54:29 -0300

    primeiro commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js,css}"],
+  theme: {
+    extend: {
+      colors: {
+        brandblue: "#0328EE",
+        brandblue2: "#1c3def",
+        brandblue3: "#0224d6",
+        whitedown: "#E5E9FD",
+      },
+    },
+  },
+  plugins: [],
+};
+
